d044eefcbba5c7c677b0622d97f396fb9f5b6f72
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-30 19:03:29 +0200

    feat(primevue): install packages for automatic component imports
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,20 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import VueRouter from 'vue-router/vite';
 import tailwindcss from '@tailwindcss/vite';
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [VueRouter(), vue(), vueDevTools(), tailwindcss()],
+    plugins: [
+        VueRouter(),
+        vue(),
+        vueDevTools(),
+        tailwindcss(),
+        Components({
+            resolvers: [PrimeVueResolver()],
+        }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
