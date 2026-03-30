9e6ea9763467a05df3b0531106110dab8a4eb5d2
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-03-30 18:43:18 +0200

    feat: install and configure vue-i18n package
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,25 @@ import '@/assets/styles/app.css';
 
 import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from 'vue-router/auto-routes';
+import { createI18n } from 'vue-i18n';
+import en from '@/locales/en.json';
+import pl from '@/locales/pl.json';
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
 
+const i18n = createI18n({
+    locale: navigator.language,
+    fallbackLocale: 'en',
+    messages: { en, pl },
+});
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount('#app');
