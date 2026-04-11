364de514252c1f85ac3e9763efa7ecc2f8a4693d
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-04-11 14:11:29 +0200

    feat(university-logo): define and use link prop
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- src/components/ui/UniversityLogo.vue
+++ src/components/ui/UniversityLogo.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import UniversityIcon from '@/components/ui/UniversityIcon.vue';
+
+defineProps({
+    link: {
+        type: String,
+        default: '/',
+    },
+});
 </script>
 
 <template>
-    <div class="flex items-center gap-3">
+    <RouterLink :to="link" class="inline-flex items-center gap-3">
         <UniversityIcon />
         <span class="text-lg font-semibold text-white">{{ $t('universityName') }}</span>
-    </div>
+    </RouterLink>
 </template>
