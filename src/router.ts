26c39fbe40bac4e472c76cfff71753b4312ac792
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-04-13 11:25:42 +0200

    feat(router): set university name as page title
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { handleHotUpdate, routes } from 'vue-router/auto-routes';
 import { useAuthStore } from '@/stores/auth.ts';
+import i18n from '@/plugins/i18n.ts';
+import type { Composer } from 'vue-i18n';
 
 export const router = createRouter({
     history: createWebHistory(),
@@ -9,6 +11,9 @@ export const router = createRouter({
 
 router.beforeEach((to) => {
     const authStore = useAuthStore();
+    const i18nGlobal = i18n.global as Composer;
+
+    document.title = i18nGlobal.t('universityName');
 
     if (to.meta.requiresGuest && authStore.isAuthenticated) {
         return '/';
