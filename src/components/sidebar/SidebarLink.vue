55055ab115c5c9c1a669f51183630b3a645edd5e
Felvesthe <23108389+Felvesthe@users.noreply.github.com>
2026-05-13 09:34:54 +0200

    fix(sidebar-link): show icon properly
    Signed-off-by: Felvesthe <23108389+Felvesthe@users.noreply.github.com>

--- src/components/sidebar/SidebarLink.vue
+++ src/components/sidebar/SidebarLink.vue
@@ -18,7 +18,7 @@ defineProps({
         exactActiveClass="bg-surface-100 dark:bg-surface-800"
         class="text-surface-700 hover:bg-surface-100 dark:text-surface-0 dark:hover:bg-surface-800 p-ripple flex cursor-pointer items-center rounded p-4 transition-colors duration-150"
     >
-        <i :class="'pi pi- mr-2' + icon"></i>
+        <i :class="'mr-2 pi pi-' + icon"></i>
         <span class="font-medium">
             <slot />
         </span>
